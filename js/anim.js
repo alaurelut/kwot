d581c700a3217774fa42d019c3fd9b8a92f11630
vqsto <romain.croizat@hotmail.fr>
2017-01-11 15:25:33 +0100

    analytic

--- js/anim.js
+++ js/anim.js
@@ -2,10 +2,12 @@ var bgImageArray = ["migrants.jpg", "tribu.jpg", "trump.jpg"],
     base = "img/",
     secs = 5;
 var textArray = [
-    "<span>La «stratégie» Twitter</span><br>du président élu Trump</span>",
-    "<span>Migrants à Calais : <br>\"Tolérance zéro\" dit la préfète </span>",
-    "<span>Photos inédites<br>d'une tribu isolée</span>"
+    "Kwot aspire les commentaires, partages des articles publiés sur les réseaux sociaux et les médias en ligne.<br><br> Il analyse et optimise votre rédaction web.",
+    "Kwot fdssdfsd les sdfsdfdsf, partdsfsdfsdfsfsfds les réseadfsdffux sociaux et les médias en ligne.<br><br> Il anasdfsdfsdf dfs dsfsd fds fsdfsdfsdf action web.",
+    "Kwot fdssdfsd les sdfsdfdsf, fdsfdsfds les réseadfsdffux ffff et les médias fdsfds fd.<br><br> Il dfsdfsd dfs dsfsd fds dfsd action web."
+   
 ];
+
 var pictoArray = [
     "picto_veille.png",
     "picto_analyse.png",
@@ -23,39 +25,24 @@ bgImageArray.forEach(function(img) {
     // caches images, avoiding white flash between background replacements
 });
 
-var fond = "no-repeat center center fixed";
 
 function backgroundSequence() {
     var k = 0;
 
     setInterval(function() {
-        $('#text').animate({ 'opacity': 0 }, 1000, function() {
-            $(this).html(textArray[k]);
+        $('#textRight').animate({ 'opacity': 0 }, 1000, function() {
+         $(this).html(textArray[k]);
+         console.log('soin');
         }).animate({ 'opacity': 1 }, 1000);
 
-        $('#picto-title').animate({ 'opacity': 0 }, 1000, function() {
-            $(this).html(pictoTitle[k]);
-        }).animate({ 'opacity': 1 }, 1000);
 
-        $('#picto-veille').animate({ 'opacity': 0 }, 1000, function() {
-            $(this).attr("src", base + pictoArray[k]);
-        }).animate({ 'opacity': 1 }, 1000);
 
-        /*        $('#absolute').animate({ 'opacity': 0 }, 500, function() {
-                    $(this).css('background', "url(" + base + bgImageArray[k] + ") no-repeat center center fixed");
-        		}).animate({ 'opacity': 1 }, 500);
-        */
-        $('html').css('background', 'url(' + base + bgImageArray[k] + ')' + fond);
-        $('html').css('-webkit-background-size', 'cover');
-        $('html').css('-moz-background-size', 'cover');
-        $('html').css('-o-background-size', 'cover');
-        $('html').css('background-size', 'cover');
         if (k + 1 == bgImageArray.length) {
             k = 0;
         } else {
             k++;
         }
-    }, 6000);
+    }, 2000);
 
 
 }
